f089121ffabf95d2e52e3720d955e2b8ba9003cd
jaina0157 <100625801+jaina0157@users.noreply.github.com>
2022-04-27 02:01:46 -0400

    Add files via upload

---
+++ functions.sql
@@ -0,0 +1,44 @@
+create or replace function payment_info (bn in number)
+return number
+is 
+    paymnt number := 0;
+    patientID number :=0;
+begin
+    select patient_id into patientID from Patient_Policy where policy_id =(select policy_id from Premium_Payments where receipt_no = (select receipt_no from Receipt where bill_no=bn));
+    select premium into paymnt from Coverage where coverage_id = (select coverage_id from Insurance_Plan where plan_id =(select plan_id from Insurance_Policy where policy_id =(select policy_id from Premium_Payments where receipt_no = (select receipt_no from Receipt where bill_no=bn))));
+    DBMS_OUTPUT.PUT_LINE('Patient ID :'|| patientID);
+    DBMS_OUTPUT.PUT_LINE('Premium :'|| paymnt);
+    return paymnt;
+exception
+    when no_data_found then
+        DBMS_OUTPUT.PUT_LINE(' no such bill number found!');
+    when others then
+        DBMS_OUTPUT.PUT_LINE('error!! try again!');     
+end;
+/
+
+
+declare 
+    b_no number;
+    premium number;
+    patientID number;
+    amt number;
+begin 
+    DBMS_OUTPUT.PUT_LINE('Please enter the bill number');
+    b_no:= &b_no;
+    DBMS_OUTPUT.PUT_LINE('Bill number :' || b_no);
+    select amount into amt from Billing where bill_no=b_no;
+    premium := payment_info(b_no);
+    If premium >= amt then
+        DBMS_OUTPUT.PUT_LINE('Insurance is covered.');
+    Else
+        DBMS_OUTPUT.PUT_LINE('Insurance premium does not cover your bill amount.');
+    End if;
+exception
+    when no_data_found then
+        DBMS_OUTPUT.PUT_LINE(' no such bill number found!');
+    when others then
+        DBMS_OUTPUT.PUT_LINE('error!! try again!');
+end;
+/
+
